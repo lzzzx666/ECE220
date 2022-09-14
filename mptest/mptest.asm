b2a36b5c079c077ebde45b710367a94de14df380
haob2 <haob2@illinois.edu>
2022-09-14 14:36:30 +0800

    MP Release

---
+++ mptest/mptest.asm
@@ -0,0 +1,1 @@
+nothing here... surprising? no, some of you guys did not use NetID to register, so you will not get this test.asm. If you get it and your friends don't, tell them to register using their netID.
